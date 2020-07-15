ee2fe1866860828c954bdf3feb90c9b082c0d2fd
mirko <mirkomorati@gmail.com>
2020-07-15 16:51:42 +0200

    Added image generator

--- utils/utils.py
+++ utils/utils.py
@@ -121,6 +121,30 @@ def human2bytes(s):
     return int(num * prefix[letter])
 
 
+import contextlib
+import joblib
+from tqdm import tqdm    
+from joblib import Parallel, delayed
+
+@contextlib.contextmanager
+def tqdm_joblib(tqdm_object):
+    """Context manager to patch joblib to report into tqdm progress bar given as argument"""
+    class TqdmBatchCompletionCallback(joblib.parallel.BatchCompletionCallBack):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+        def __call__(self, *args, **kwargs):
+            tqdm_object.update(n=self.batch_size)
+            return super().__call__(*args, **kwargs)
+
+    old_batch_callback = joblib.parallel.BatchCompletionCallBack
+    joblib.parallel.BatchCompletionCallBack = TqdmBatchCompletionCallback
+    try:
+        yield tqdm_object
+    finally:
+        joblib.parallel.BatchCompletionCallBack = old_batch_callback
+        tqdm_object.close()
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
